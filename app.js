3b23fddc1c4fbbab626d0ee74ec76e3b8266583b
jpalmieri <jpalmieri@lumoslabs.com>
2015-09-15 13:44:40 -0700

    overhaul and refactor

--- app.js
+++ app.js
@@ -1,68 +1,77 @@
 (function() {
 
+  var MACROS_URI = '/api/v2/macros.json';
+
   return {
     events: {
-      'app.activated':'doSomething',
-      'click #stringButton': 'doSearch',
-      'searchMacros.done': 'handleResults',
-      'getNextPage.done': 'handleResults'
+      'app.activated':                      'initialize',
+      'click .tagButton':                   'tagSearch',
+      'requestMacrosForTagSearch.done':     'filterTagResults'
     },
 
     requests: {
-      searchMacros: function() {
+      requestMacrosForTagSearch: function(url) {
         return {
-          url: '/api/v2/macros.json',
+          url: url || MACROS_URI,
           type: 'GET',
           dataType: 'json'
         };
       },
-
-      getNextPage: function(url) {
-        return {
-          url: url,
-          type: 'GET',
-          dataType: 'json'
-        };
-      }
     },
 
-    doSomething: function() {
+    initialize: function() {
       this.switchTo('search');
     },
 
-    doSearch: function() {
+    startSearch: function() {
       this.$('.results ul').empty();
-      this.$('#stringButton').prop('disabled', true);
-      this.$('#stringButton').prop('value', 'Searching...');
-      this.ajax('searchMacros');
+      this.$('.tagButton').prop('disabled', true);
+      this.$('.tagButton').prop('value', 'Searching...');
       return false;
     },
 
-    handleResults: function (data) {
+    tagSearch: function() {
+      this.startSearch();
+      this.ajax('requestMacrosForTagSearch');
+    },
+
+    filterTagResults: function(data) {
       console.log(data);
-      var macros = data.macros;
+      var self = this;
       var results = [];
       var query = this.$("#search").val();
-      var that = this;
-
+      var macros = data.macros;
       macros.forEach( function(macro) {
-        results.push( that.getTagMatches(macro, query) );
+        var tags = self.getValues(macro);
+        if ( tags.indexOf(query) > -1 ) {
+          results.push(macro);
+        }
       });
 
+      this.displayResults(results);
+
+      // Get additional pages of api request results
+      if (data.next_page){
+        this.ajax('requestMacrosForTagSearch', data.next_page);
+      } else {
+        this.$('.tagButton').prop('disabled', false);
+        this.$('.tagButton').prop('value', 'Search Macros');
+      }
+    },
+
+    displayResults: function (results) {
       // Render the template
       var resultsTemplate = this.renderTemplate('results', {results: results} );
 
       // Insert rendered template into the results div
       this.$('.results ul').append(resultsTemplate);
 
-      if (data.next_page){
-        this.ajax('getNextPage', data.next_page);
-      } else {
-        this.$('#stringButton').prop('disabled', false);
-        this.$('#stringButton').prop('value', 'Search Macros');
-      }
+      // Display result count
       this.$('.count').text("Displaying " + this.$('.results ul li').length + " matches");
+
     },
+
+    // Helpers
 
     getMacroActions: function(macro) {
       var actions = [];
@@ -81,12 +90,6 @@
       return values;
     },
 
-    getTagMatches: function(macro, query) {
-      var tags = this.getValues(macro);
-      if ( tags.indexOf(query) > -1 ) {
-        return macro;
-      }
-    }
   };
 
 }());
